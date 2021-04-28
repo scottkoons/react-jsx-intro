76a880d4500716fb724c5db47e11b27845a2399f
scottkoons <scottkoons@gmail.com>
2021-04-27 21:40:06 -0600

    Part 1

---
+++ part-1/index.js
@@ -0,0 +1,22 @@
+const FirstComponent = () => {
+    return (
+        <h1>My very first component</h1>
+    );
+};
+
+const NamedComponent = (props) => {
+    return (
+        <p>My name is {props.name}</p>
+    );
+};
+
+const App = () => {
+    return (
+        <div>
+            < FirstComponent />
+            < NamedComponent name="Ned Stark" />
+        </div>
+    );
+};
+
+ReactDOM.render(< App />, document.getElementById('root'));
